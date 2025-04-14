5f486464f15bf3db92f5b03642fdc6dfaf265b9b
shafna <shafnafatima248@gmail.com>
2025-04-14 11:24:47 +0530

    commit new

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors({origin:'*'}))
   const appPort = process.env.NOTE_PORT ?? 8080
   await app.listen(appPort );
   console.log(`This service is running in the port 'http://localhost:${appPort}`)
